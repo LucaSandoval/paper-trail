f6a1e99141db2df4631a701f9271334fcfdeeffc
wangwang333 <128167566+wangwang333@users.noreply.github.com>
2024-11-21 23:28:47 -0500

    buzz&click

---
+++ Paper Trail/Assets/Scripts/Buzzlight.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buzzlight : MonoBehaviour
+{
+    public Light spotlight; 
+    public AudioSource buzzAudio; 
+
+    // Flash happens every 10 seconds, and for each flash the light flicker 3-4 times
+    public float flashInterval = 0.1f; 
+    public int minCount = 3; 
+    public int maxCount = 4; 
+    public float flashCooldown = 10f; 
+    private bool isFlashing = false;
+    
+    void Start()
+    {
+        InvokeRepeating(nameof(StartFlash), 0f, flashCooldown);
+    }
+
+    public void StartFlash()
+    {
+        if (!isFlashing)
+        {
+            StartCoroutine(flashRoutine());
+        }
+    }
+
+    private IEnumerator flashRoutine()
+    {
+        isFlashing = true;
+        //buzz either 3 or 4 times
+        int flashCount = Random.Range(minCount, maxCount + 1);
+        for (int i = 0; i < flashCount; i++)
+        {
+            if (i % 2 == 0)
+            {
+                buzzAudio.Play();
+            }
+            spotlight.intensity = Random.Range(0.5f, 2.2f);
+            yield return new WaitForSeconds(flashInterval);
+        }
+
+
+        // Return to default
+        spotlight.intensity = 2.2f;
+        isFlashing = false;
+    }
+
+}
